ead8eb3ab7bd42f8cdd979d7146eb05b7d33f43c
Iron-E <code.iron.e@gmail.com>
2022-07-10 18:09:15 -0400

    chore: initial commit
    The rest of the git history is at:
    
    * https://github.com/Iron-E/clinvoice/tree/cbe77bc76abd24c0f4dbf75219aa8b721e4bc6c8/crates/clinvoice_error

---
+++ README.md
@@ -0,0 +1,3 @@
+# `clinvoice_error`
+
+`clinvoice_error` is a library written for [CLInvoice](https://github.com/Iron-E/clinvoice/).
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! This crate adds macros which generate boilerplate code associated with [`Error`] and [`Result`]
+//! types.
+
+#![macro_use]
+
+mod alias_result;
